8e5db6190c103ec3d2decdf56811e961135bbdb1
LNigam <LNigam@VED-LT-090>
2019-10-07 15:33:18 +0530

    unroll new changes for font size scroller

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,12 +1,4 @@
 
-$(function () {
-  $("#fontSizeScroller").slider({
-    step: 3,
-    value: 14,
-    min: 5,
-    max: 23,
-    change: function (event, ui) {
-      $("body").css("font-size", ui.value + "px")
-    }
-  });
-});
+$( function() {
+    $( "#fontSizeScroller" ).slider();
+  } );
