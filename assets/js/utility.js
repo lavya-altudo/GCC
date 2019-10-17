56aefe0ab678348fe89d3827def15089db88f1f9
LNigam <LNigam@VED-LT-090>
2019-10-17 15:47:37 +0530

    212009: Choose A month: Trip Extension content box

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,5 +1,12 @@
 
-$( function() {
-    $( "#fontSizeScroller" ).slider();
-    $(".dateprice-dropdown select").selectmenu();
-  } );
+$(function () {
+  $("#fontSizeScroller").slider();
+  $(".select-trip-month-title").click(function () {
+    $(this).closest(".select-trip-month-row").toggleClass("open").siblings().removeClass('open');
+  })
+  $(".select-trip-date-title").click(function () {
+    $(this).closest(".select-trip-date-row").toggleClass("open").siblings().removeClass('open');
+  })
+
+  $(".dateprice-dropdown select").selectmenu();
+});
